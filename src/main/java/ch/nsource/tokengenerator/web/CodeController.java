50656b162b5a2082a078a1d027a78f4b39c8bbcb
Noah Schaufelberger <noah@nsource.ch>
2026-01-06 20:25:51 +0100

    Enable Cross-Origin Resource Sharing (CORS) for CodeController

--- src/main/java/ch/nsource/tokengenerator/web/CodeController.java
+++ src/main/java/ch/nsource/tokengenerator/web/CodeController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/codes")
 public class CodeController {
     private final CodeService codeService;
